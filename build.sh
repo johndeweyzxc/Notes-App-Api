fd6053c90d02b29b7ba6e958c3d76a10427fb584
johndeweyzxc <johndewey02003@gmail.com>
2023-08-21 10:51:39 +0800

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar -czvf build/note_taking_fast_api.tar.gz dockerized
+tar -czvf build/note_taking_fast_api.tar.gz dockerized/note_taking_fast_api dockerized/docker-compose.yaml dockerized/Dockerfile
